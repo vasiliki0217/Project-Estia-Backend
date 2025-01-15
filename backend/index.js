3b281c360f95f0e7aca78a235a5b44f3148e2cda
vasiliki0217 <korai.vasiliki@gmail.com>
2025-01-15 16:34:49 +0100

    routes corrections

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ const usersRoutes = require('./routes/users')
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/business', businessRoutes);
-app.use('/users', usersRoutes);
+app.use('/api/v1/business', businessRoutes);
+app.use('api/v1/users', usersRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
