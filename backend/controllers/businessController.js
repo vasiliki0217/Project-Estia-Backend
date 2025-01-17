2b4c5bdd17eb3b69ed850337589c1b720e39e7d6
vasiliki0217 <korai.vasiliki@gmail.com>
2025-01-17 13:57:51 +0100

    endpoint for business with address added

--- backend/controllers/businessController.js
+++ backend/controllers/businessController.js
@@ -25,7 +25,7 @@ const getBusinessById = async (req, res) => {
 const getBusinessAddressId = async (req, res) => {
     const { id } = req.params;
     try {
-        const result = await pool.query('SELECT id_Address FROM Business WHERE id_Address = $1', [id]);
+        const result = await pool.query('SELECT id_Address FROM Business WHERE id= $1', [id]);
         if (result.rows.length === 0) {
             return res.status(404).json({ error: 'Business not found' });
         }
--- backend/routes/business.js
+++ backend/routes/business.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getBusiness, getBusinessById, getBusinessAddressId } = require('../controllers/businessController');
+const { getBusiness, getBusinessById, getBusinessWithAddress } = require('../controllers/businessController');
 
 
 router.get('/', getBusiness);
 router.get('/:id', getBusinessById);
-router.get('/:id/id_Address', getBusinessAddressId);
+router.get('/:id/address_details', getBusinessWithAddress);
 
 module.exports = router;
