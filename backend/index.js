5f5d7dff3f59dee5f93ea671578504b5cd8713f1
vasiliki0217 <korai.vasiliki@gmail.com>
2025-01-22 15:51:18 +0100

    registerUser added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const businessRoutes = require('./routes/business');
 const usersRoutes = require('./routes/users');
@@ -8,9 +7,23 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// const allowedOrigins = [
+//     'http://localhost:3000',
+//     '--frontend url--'
+// ];
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
+//to limit the access
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Estia Project API!');
@@ -24,4 +37,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-//app.listen(process.env.Port || 3000)
+
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUserById } = require('../controllers/usersController');
+const { getUsers, getUserById, registerUser } = require('../controllers/usersController');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
-// router.post('/register', registerUser);
+router.post('/register', registerUser);
 // router.post('/login', loginUser);
 
 module.exports = router;
